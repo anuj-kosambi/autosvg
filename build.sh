ae5ba6e314c793ce5dc455a3578b610042ae4fbf
anujkosambi <anujkosambi@gmail.com>
2020-06-02 19:34:53 +0530

    [build] adding automatic build fixes

--- build.sh
+++ build.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-export EMSDK=./third_party/emsdk
-export OPENCV_SDK=/third_party/opencv
-export NUMCPP=/third_party/NumCpp
+export EMSDK=`pwd`/third_party/emsdk
+export OPENCV_SDK=`pwd`/third_party/opencv
+export NUMCPP=`pwd`/third_party/NumCpp
+export EMSCRIPTEN=$EMSDK/upstream/emscripten/
 
 $EMSDK/emsdk update-tags
 python $EMSDK/emsdk.py install latest
 python $EMSDK/emsdk.py activate latest
 source $EMSDK/emsdk_env.sh
 
+cd $OPENCV_SDK
+python ./platforms/js/build_js.py build_wasm --build_wasm
+
+cd .. && cd ..
 cmake -DCMAKE_TOOLCHAIN_FILE=${EMSDK}/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake
+
+echo $OPENCV_SDK
+make

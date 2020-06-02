2c25f3c6796450e7caba3854c2159c1cbcbebb52
anujkosambi <anujkosambi@gmail.com>
2020-06-02 17:21:20 +0530

    [build] [ci skip] adding submodules

--- build.sh
+++ build.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+
+export EMSDK=./third_party/emsdk
+export OPENCV_SDK=/third_party/opencv
+export NUMCPP=/third_party/NumCpp
+
+$EMSDK/emsdk update-tags
+python $EMSDK/emsdk.py install latest
+python $EMSDK/emsdk.py activate latest
+source $EMSDK/emsdk_env.sh
+
 cmake -DCMAKE_TOOLCHAIN_FILE=${EMSDK}/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake
